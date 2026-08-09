400eb84f3761330cc2dc0304ad1ee69fc54bd603
vjgamer-94 <vjgamerprajapat@gmail.com>
2026-08-09 19:19:36 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
       {
         name: "MADE FOR THE OG", // required by Discord API, not shown in the client
         state: "MADE FOR THE OG",     // this is what people actually see
-        type: 1,               // Custom
+        type: 4,               // Custom
       },
     ],
   },
